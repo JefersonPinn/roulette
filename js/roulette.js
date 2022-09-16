5de6e7a3ae2994794c755e796bc6e726c196bdf2
JefersonPinn <113300091+JefersonPinn@users.noreply.github.com>
2022-09-16 00:25:48 -0300

    updating

--- js/roulette.js
+++ js/roulette.js
@@ -3,6 +3,7 @@ const language = 'pt-BR';
 const URL = `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=${language}&region=BR&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=23&with_genres=53&with_watch_monetization_types=flatrate`;
 const Filme = document.getElementById('filme');
 
+
 function gerarNum(a, b) {
     return Math.floor(Math.random() * (b-a + 1)) + a;
 }
@@ -15,36 +16,37 @@ function roletar(idGenre){
         }).then(jsonParsed =>{
                 const pages = jsonParsed.total_pages;
                 const results = jsonParsed.total_results;
-                const pageNumber = Math.floor(Math.random() * 20 + 1)
-                
-                getMovie();
-                function getMovie() {
-                    const Movie = `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=${language}&region=BR&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=1&with_genres=${idGenre}&with_watch_monetization_types=flatrate`;
-                    fetch(Movie
-                        ).then(response => {
-                            return response.json();
-                        }).then(jsonres =>{
-                            const movieNum = jsonres.results[gerarNum(0, 19)]
-                            console.log(jsonres);
-                            const nomeFilme = movieNum.original_title;
-                            const posterFilme = movieNum.poster_path;
-                            const overviewFilme = movieNum.overview;
-                            createDivMovie(nomeFilme, Filme, posterFilme, overviewFilme);
-                            /*jsonres.results.forEach(movie =>{
-                                const nomeFilme = movie.original_title;
-                                const posterFilme = movie.poster_path;
-                                const overviewFilme = movie.overview;
-                                createDivMovie(nomeFilme, Filme, posterFilme, overviewFilme);
-                                console.log(movie);
-                            })*/
-                            console.log(gerarNum(0, 19));
-                        } )
-                }
-        } )
+                getMovie(idGenre);
+            } )
         while(Filme.firstChild){
             Filme.removeChild(Filme.firstChild);
         }
+        
 }
+
+function getMovie(idGenre) {
+    const Movie = `https://api.themoviedb.org/3/discover/movie?api_key=${KEY}&language=${language}&region=BR&sort_by=primary_release_date.desc&include_adult=false&include_video=false&page=${gerarNum(0, 19)}&with_genres=${idGenre}&with_watch_monetization_types=flatrate`;
+    fetch(Movie
+        ).then(response => {
+            return response.json();
+        }).then(jsonres =>{
+            const movieNum = jsonres.results[gerarNum(0, 19)]
+            console.log(jsonres);
+            const nomeFilme = movieNum.original_title;
+            const posterFilme = movieNum.poster_path;
+            const overviewFilme = movieNum.overview;
+            createDivMovie(nomeFilme, Filme, posterFilme, overviewFilme);
+            /*jsonres.results.forEach(movie =>{
+                const nomeFilme = movie.original_title;
+                const posterFilme = movie.poster_path;
+                const overviewFilme = movie.overview;
+                createDivMovie(nomeFilme, Filme, posterFilme, overviewFilme);
+                console.log(movie);
+            })*/
+            console.log(gerarNum(0, 19));
+        } )
+}
+
 
 function createDivMovie(name, div, poster_path, overview) {
     const divPai = document.createElement('div');
